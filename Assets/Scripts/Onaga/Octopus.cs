cf1ffc7c538a959f4ee7f018549000a473933370
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-12-12 15:35:25 +0900

    タコ＆ウニ修正

---
+++ Assets/Scripts/Onaga/Octopus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octopus : EnemyBase {
+
+	// Use this for initialization
+	void Start () {
+        base.enemySpeed = 0.5f;
+        base.enemyPositionX = transform.position.x;
+        base.enemyPositionY = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
