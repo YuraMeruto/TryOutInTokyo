87a69be2531ab6b13d9038235e3febcd62059f5c
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-12-12 11:14:10 +0900

    敵：ウニ（簡易版）ついか

---
+++ Assets/Scripts/Onaga/urchin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urchin : EnemyBase {
+
+    private float ActiveTimer;
+
+    public GameObject urchinActive;
+
+    int status;
+
+    // Use this for initialization
+    void Start () {
+        ActiveTimer = 0.0f;
+        status = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+      switch (status)
+        {
+            case 0:
+                ActiveTimer += Time.deltaTime;
+                if (ActiveTimer >= 2.0f)
+                {
+                    urchinActive.SetActiveRecursively(true);
+                    status = 1;
+                }
+                break;
+            case 1:
+                ActiveTimer -= Time.deltaTime;
+                if (ActiveTimer <= 0.0f)
+                {
+                    urchinActive.SetActiveRecursively(false);
+
+                    status = 0;
+                }
+                break;
+
+        }
+    }
+}
