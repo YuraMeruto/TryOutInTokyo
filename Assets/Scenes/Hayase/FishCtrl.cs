23667cc40cb86b1eb92ddbec812fda1c38173fe9
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2017-12-12 10:41:19 +0900

    インスペクターでも構えるように変更

--- Assets/Scenes/Hayase/FishCtrl.cs
+++ Assets/Scenes/Hayase/FishCtrl.cs
@@ -2,24 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishCtrl : MonoBehaviour {
+public class FishCtrl : EnemyBase {
+
+    // 管理用座標
+    Vector2 pos;
 
     // 魚の移動方向
     Vector3 direction = Vector3.left;
 
-    // 魚の移動速度
-    float moveSpeed = 0.5f;
+    // 移動速度初期値
+    float moveInitializeSpeed = 0.5f;
+
+    [SerializeField, Tooltip("敵のスケール")]
+    private float enemyScale = 0.8f;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
+        // 初期値
+        enemyPositionX = transform.position.x;
+        enemyPositionY = transform.position.y;
+
+        // サイズ変更
+        transform.localScale = new Vector3(enemyScale,enemyScale);
+
+        // 速度設定
+        enemySpeed = moveInitializeSpeed;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        EnemyMove();
 
         // 削除
         if (transform.position.x < -10) Destroy(gameObject);
 	}
+
+    public override void EnemyMove()
+    {
+        // 移動処理
+        transform.position = pos;
+
+        // 編集
+        enemyPositionX -= enemySpeed * Time.deltaTime;
+        pos = new Vector2(enemyPositionX, enemyPositionY);
+    }
 }
