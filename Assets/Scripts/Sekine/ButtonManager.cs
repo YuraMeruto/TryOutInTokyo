ed4119edc56eec21529ce3b1d43df3feab4c2a3f
Sekine Akira <ekusika834@gmail.com>
2017-12-12 11:23:06 +0900

    シーン遷移仮完成

---
+++ Assets/Scripts/Sekine/ButtonManager.cs
@@ -0,0 +1,29 @@
+﻿////////////////////////////////////
+//製作者　関根明良
+//クラス　ボタン処理クラス
+////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+	/// <summary>
+	/// シーン遷移
+	/// </summary>
+	public void SceneTransition(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+	/// <summary>
+	/// メインシーンへの遷移
+	/// </summary>
+	/// <param name="stageNum">遷移先のステージ番号</param>
+	public void ToGameScene(int stageNum)
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
