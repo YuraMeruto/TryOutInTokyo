17d4c7d80b90e1fd37f763545c1f7bf80e4bcc87
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2017-12-12 10:03:24 +0900

    魚の移動処理

---
+++ Assets/Scenes/Hayase/FishCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishCtrl : MonoBehaviour {
+
+    // 魚の移動方向
+    Vector3 direction = Vector3.left;
+
+    // 魚の移動速度
+    float moveSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+        // 削除
+        if (transform.position.x < -10) Destroy(gameObject);
+	}
+}
