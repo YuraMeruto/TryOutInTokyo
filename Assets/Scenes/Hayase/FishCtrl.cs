9c0c8b737a34920ba2679dbed498b9217f436785
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2017-12-12 11:11:01 +0900

    初期値設定メソッド作成

--- Assets/Scenes/Hayase/FishCtrl.cs
+++ Assets/Scenes/Hayase/FishCtrl.cs
@@ -14,21 +14,16 @@ public class FishCtrl : EnemyBase {
     float moveInitializeSpeed = 0.5f;
 
     [SerializeField, Tooltip("敵のスケール")]
-    private float enemyScale = 0.8f;
+    private float enemyScale;
 
 	// Use this for initialization
 	void Start () {
-        // 初期値
-        enemyPositionX = transform.position.x;
-        enemyPositionY = transform.position.y;
-
-        // 速度設定
-        enemySpeed = moveInitializeSpeed;
+        InitialSettings(0.8f, moveInitializeSpeed, transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
     void Update () {
-        // デバック用？
+        //デバック用？
         // サイズ変更
         transform.localScale = new Vector3(enemyScale, enemyScale);
 
@@ -46,5 +41,25 @@ public class FishCtrl : EnemyBase {
         // 編集
         enemyPositionX -= enemySpeed * Time.deltaTime;
         pos = new Vector2(enemyPositionX, enemyPositionY);
+    }
+
+    /// <summary>
+    /// 初期設定
+    /// </summary>
+    /// <param name="scale">初期サイズ</param>
+    /// <param name="speed">初期速度</param>
+    /// <param name="x">初期X座標</param>
+    /// <param name="y">初期Y座標</param>
+    private void InitialSettings(float scale, float speed, float x, float y)
+    {
+        // 初期値
+        enemyPositionX = x;
+        enemyPositionY = y;
+
+        // 速度設定
+        enemySpeed = speed;
+
+        // サイズ
+        enemyScale = scale;
     }
 }
