5a8759120d9130222b815140a9b8021491f1dc75
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-12-12 09:49:11 +0900

    EnemyBase

---
+++ Assets/Scripts/Onaga/EnemyBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class EnemyBase : MonoBehaviour {
+
+    public int EnemyType;
+    public int EnemyHp;
+    public int EnemyAttac;
+    public float EnemySpeed;
+    public float EnemyPositionX;
+    public float EnemyPositionY;      
+
+    /// <summary>
+    /// 敵移動
+    /// </summary>
+    public virtual void EnemyMove()
+    {
+        
+    }
+}
