1b92547499e315c78a47f6883212c8662924019a
YamaguchiShuhei <unaginimono@gmai.com>
2017-12-12 15:32:26 +0900

    ふじつぼに当たっている状態でのみ動かす実装

--- Assets/Scripts/Yamaguchhi/ArrowAction.cs
+++ Assets/Scripts/Yamaguchhi/ArrowAction.cs
@@ -17,13 +17,11 @@ public class ArrowAction : MonoBehaviour {
         //矢印の回転
         if (Input.mousePosition.x < Camera.main.WorldToScreenPoint(transform.position).x)
         {
-            Debug.Log("a");
             Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 rot = (vec - new Vector2(transform.position.x, transform.position.y)).normalized;
             transform.rotation = Quaternion.FromToRotation(Vector3.up, rot);
         }else if(Input.mousePosition.x >= Camera.main.WorldToScreenPoint(transform.position).x)
         {
-            Debug.Log("b::"+"mouse:"+Input.mousePosition.x+" player:"+transform.position.x);
             Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 rot = (-vec - new Vector2(transform.position.x, transform.position.y)).normalized;
             transform.rotation = Quaternion.FromToRotation(Vector3.up, rot);
--- Assets/Scripts/Yamaguchhi/PlayerContoller.cs
+++ Assets/Scripts/Yamaguchhi/PlayerContoller.cs
@@ -7,12 +7,11 @@ public class PlayerContoller : MonoBehaviour {
     /***********************************************
      * クラス変数
      * ********************************************/
-    private Rigidbody2D myRigidbody;
+    //private Rigidbody2D myRigidbody;
     private Vector2 playerVec;    
     private Vector2 clickPosDown, clickPosUp;
     private float power = 10.0f;
     private float pullDistance;
-    private bool isAction = false;
     private GameObject arrow;
     
 
@@ -21,7 +20,7 @@ public class PlayerContoller : MonoBehaviour {
      * ********************************************/
     void Awake()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
+        //myRigidbody = GetComponent<Rigidbody2D>();
         arrow = transform.Find("Arrow").gameObject;
     }
     // Use this for initialization
@@ -32,42 +31,41 @@ public class PlayerContoller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButtonDown(0))
+        if (PlayerManager.Instance.IsAction)
         {
-            //タッチした場所のポジション
-            clickPosDown = Input.mousePosition;
-            Debug.Log("Down:" + clickPosDown);
-            arrow.SetActive(true);
-            
-            
-        }
-
-        
-        
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            //離した場所のポジション
-            clickPosUp = Input.mousePosition;
-            Debug.Log("Up:" + clickPosUp);
-            arrow.SetActive(false);
-
-            if(clickPosDown == clickPosUp)
+            if (Input.GetMouseButtonDown(0))
             {
-                //指を離した場所が同じ場所なら処理抜ける
-                return;
+                //タッチした場所のポジション
+                clickPosDown = Input.mousePosition;
+                arrow.SetActive(true);
+
             }
 
-            
-            
+            if (Input.GetMouseButtonUp(0))
+            {
+                //離した場所のポジション
+                clickPosUp = Input.mousePosition;
+                arrow.SetActive(false);
 
-            //飛ばす方向の計算
-            playerVec = clickPosDown - clickPosUp;       
-            playerVec.Normalize();
+                if (clickPosDown == clickPosUp)
+                {
+                    //指を離した場所が同じ場所なら処理抜ける
+                    return;
+                }
 
-            pullDistance = (clickPosDown - clickPosUp).magnitude;
-            Debug.Log(pullDistance);
-            myRigidbody.AddForce(playerVec * power*pullDistance);
+
+
+
+                //飛ばす方向の計算
+                playerVec = clickPosDown - clickPosUp;
+                playerVec.Normalize();
+                //ドラッグした距離の計算
+                pullDistance = (clickPosDown - clickPosUp).magnitude;
+                //飛ばす処理
+                PlayerManager.Instance.MyRigidbody.AddForce(playerVec * power * pullDistance);
+                PlayerManager.Instance.IsAction = false;
+            }
+        
         }
 	}
     
