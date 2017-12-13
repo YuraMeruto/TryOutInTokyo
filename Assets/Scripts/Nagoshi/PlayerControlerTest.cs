5c2904aece60d3232106451c36a2216c77c7e87d
YUUTO\大樹 <yakmsy.memories@ezweb.ne.jp>
2017-12-14 08:43:22 +0900

    プレイヤーのエラーの解消

---
+++ Assets/Scripts/Nagoshi/PlayerControlerTest.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControlerTest : MonoBehaviour
+{
+    /***********************************************
+     * クラス変数
+     * ********************************************/
+    private Vector2 playerVec; //飛ばす角度
+    private float power; // 飛ばす力
+    [SerializeField, Tooltip("飛ばす力の最大値")]
+    private float maxPower = 500.0f;
+    [SerializeField, Tooltip("飛ばす力の最小値")]
+    private float minPower = 200.0f;
+    private Vector2 clickPosDown, clickPosUp; //ドラッグしたポジション  
+    private float pullDistance; // ドラッグした距離
+    private GameObject arrow; // 矢印
+    [SerializeField, Tooltip("SEを鳴らすAudio")]
+    private AudioSource audio;
+    [SerializeField]
+    private AudioClip[] clip;
+    private Slider meter;
+    [SerializeField, Tooltip("PlayerMangerTestのスクリプト")]
+    PlayerManagerTest playermanagerScript;
+    /***********************************************
+     * クラス関数
+     * ********************************************/
+    void Start()
+    {
+        arrow = transform.Find("Arrow").gameObject;
+        meter = GameObject.Find("Canvas/Meter").GetComponent<Slider>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (playermanagerScript.IsAction)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //タッチした場所のポジション
+                clickPosDown = Input.mousePosition;
+                arrow.SetActive(true);
+                SePlay(0);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                //離した場所のポジション
+                clickPosUp = Input.mousePosition;
+                arrow.SetActive(false);
+
+                if (clickPosDown == clickPosUp)
+                {
+                    //指を離した場所が同じ場所なら処理抜ける
+                    return;
+                }
+
+                //飛ばす方向の計算
+                playerVec = clickPosDown - clickPosUp;
+                playerVec.Normalize();
+                //ドラッグした距離の計算
+                pullDistance = (clickPosDown - clickPosUp).magnitude;
+
+                power = pullDistance * 3.0f;
+                Debug.Log(power);
+
+                if (power >= maxPower)
+                {
+                    power = maxPower;
+                }
+                else if (power < minPower)
+                {
+                    return;
+                }
+                //飛ばす処理
+                playermanagerScript.MyRigidbody.AddForce(playerVec * power);
+                playermanagerScript.IsAction = false;
+                SePlay(1);
+            }
+        }
+
+        meter.value = transform.position.x;
+    }
+
+    public void SePlay(int number)
+    {
+        audio.clip = clip[number];
+        audio.Play();
+    }
+}
---
+++ Assets/Scripts/Nagoshi/PlayerManagerTest.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManagerTest : MonoBehaviour {
+
+    /*******************************************************
+     * クラス変数
+     * ****************************************************/
+    private bool isAction = false;
+    [SerializeField, Tooltip("プレイヤーのRigidBody2D")]
+    private Rigidbody2D myRigidbody;
+    private Vector2 direction;
+    [SerializeField, Tooltip("プレイヤーのAnimator")]
+    private Animator anim;
+    private Animator hujiAnim;
+    [SerializeField, Tooltip("プレイヤーPlayerFlowのAnimator")]
+    private PlayerFlow flow;
+    private GameObject gameOverPanel;
+    private GameObject loadManager;
+    [SerializeField, Tooltip("PlayerControlerのスクリプト")]
+    private PlayerControlerTest playerControlerScript;
+    /******************************************************
+     * プロパティ
+     * ***************************************************/
+
+    public bool IsAction
+    {
+        get { return isAction; }
+        set { isAction = value; }
+    }
+    public Rigidbody2D MyRigidbody
+    {
+        get { return myRigidbody; }
+    }
+    /******************************************************
+     * クラス関数
+     * ***************************************************/
+    void Start()
+    {
+        loadManager = GameObject.Find("Manager/LoadManager");
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        switch (collider.gameObject.tag)
+        {
+            case "Wall":
+                playerControlerScript.SePlay(3);
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.tag);
+        switch (collider.gameObject.tag)
+        {
+
+            case "Hujitsubo":
+                Hujitsubo(collider.gameObject.transform);
+                playerControlerScript.SePlay(2);
+                break;
+            case "Goal":
+                myRigidbody.velocity = Vector2.zero;
+                SceneManager.LoadScene("Result");
+                break;
+            case "Enemy":
+                switch (collider.gameObject.name)
+                {
+                    case "Fish":
+                        playerControlerScript.SePlay(5);
+                        break;
+                    case "Urchin":
+                        playerControlerScript.SePlay(4);
+                        break;
+                }
+                loadManager.GetComponent<StageGeneration>().ShowPanel();
+                break;
+            default:
+                //audio.clip = clip[1];
+                //audio.Play();
+                break;
+        }
+        myRigidbody.simulated = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Hujitsubo")
+        {
+            flow.isFlow = true;
+            hujiAnim.speed /= 1.5f;
+            anim.SetBool("IsPlayer", true);
+        }
+    }
+
+    /// <summary>
+    /// ふじつぼ処理
+    /// </summary>
+    /// <param name="pos">ふじつぼのTransform</param>
+    void Hujitsubo(Transform pos)
+    {
+        //audio.clip = clip[0];
+        //audio.Play();
+        hujiAnim = pos.gameObject.GetComponent<Animator>();
+        hujiAnim.speed *= 1.5f;
+
+        anim.SetBool("IsPlayer", false);
+        flow.isFlow = false;
+        isAction = true;
+        myRigidbody.velocity = Vector2.zero;
+        myRigidbody.simulated = false;
+        transform.rotation = Quaternion.identity;
+        transform.position = pos.position;
+    }
+    //-------------------------------------
+
+
+}
