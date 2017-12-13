d2f0abcd95e5e62786e7124972f1589a3066f9ac
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-12-13 10:40:53 +0900

    ウニのDestroy追加

--- Assets/Scripts/Onaga/urchin.cs
+++ Assets/Scripts/Onaga/urchin.cs
@@ -6,27 +6,47 @@ public class Urchin : EnemyBase
 {
 
     private float activeTimer;
-
+    private float distance;
     private int status;
 
     Animator urchinAnimation;
     public GameObject urchinActive;
     public BoxCollider2D collider;
-    //ウニのサイズ・座標
-    Vector2 ActiveColliderOffset = new Vector2(-0.51f, 0.56f);
-    Vector2 ActiveColliderSize = new Vector2(7.846f, 7.86f);
+    public GameObject mainCamera;
 
-    Vector2 DefalutColliderOffset = new Vector2(1.81f, 0.22f);
-    Vector2 DefalutColliderSize = new Vector2(5.58f, 5.16f);
+    //ウニのサイズ・座標
+    private Vector2 ActiveColliderOffset = new Vector2(-0.51f, 0.56f);
+    private Vector2 ActiveColliderSize = new Vector2(7.846f, 7.86f);
+
+    private Vector2 DefalutColliderOffset = new Vector2(1.81f, 0.22f);
+    private Vector2 DefalutColliderSize = new Vector2(5.58f, 5.16f);
+
+    private Vector2 uchinPosition;
+    private Vector2 cameraPosition;
+
     void Start()
     {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
         urchinAnimation = GetComponent<Animator>();
         activeTimer = 0.0f;
         status = 0;
+        distance = 0.0f;
+        uchinPosition = transform.position;
+        //カメラオブジェクト取得
+
+
     }
 
     void Update()
     {
+        cameraPosition = mainCamera.transform.position;
+        //distance = Vector2.Distance(uchinPosition, cameraPosition);
+        //distance = uchinPosition.x - cameraPosition.x;
+        Debug.Log("distance" + (uchinPosition.x - cameraPosition.x));
+        if(uchinPosition.x - cameraPosition.x < -10.0f)
+        {
+            Destroy(gameObject);
+        }
         switch (status)
         {
             case 0:
