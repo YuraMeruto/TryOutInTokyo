26a7c66869113f8a016a022348edc293b239d743
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-12-13 16:41:37 +0900

    Titleフェードアウト

---
+++ Assets/Scripts/Onaga/OFade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OFade : MonoBehaviour
+{
+    private bool fadeFlg;
+    private float alfa = 0.0f;
+    float red, green, blue;
+
+    //public float fadeSpeed = 0.f;
+
+    // Use this for initialization
+    void Start()
+    {
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            fadeFlg = true;
+        }
+        if(fadeFlg)
+        {
+            FadePanel();
+        }
+    }
+    public void FadePanel()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        alfa += Time.deltaTime;
+        Debug.Log("alfa" + alfa);
+    }
+}
