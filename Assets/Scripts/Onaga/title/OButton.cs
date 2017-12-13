3a9e27a479be51a0c0816ed875d1f336f3188815
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-12-13 20:29:05 +0900

    TitleSE追加

--- Assets/Scripts/Onaga/title/OButton.cs
+++ Assets/Scripts/Onaga/title/OButton.cs
@@ -5,16 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class OButton : MonoBehaviour
 {
+    private AudioSource touchSE;
     public OFade fade;
     private float SceneChengePoint = 1.0f;
     private float fadeWiteTime = 0.0f;
     private bool sceneFlg = false;
     public float fadeSpeed;
 
+    void Start()
+    {
+        touchSE = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
+            touchSE.PlayOneShot(touchSE.clip);
             fadeSpeed = fade.fadeSpeed;
             sceneFlg = true;
         };
