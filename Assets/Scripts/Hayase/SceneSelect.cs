0b46b0fc359e99d0827ca2ace3fc8454a2366547
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2017-12-14 00:59:36 +0900

    SelectScene一部追加

---
+++ Assets/Scripts/Hayase/SceneSelect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSelect : MonoBehaviour {
+
+    [SerializeField]
+    GameObject panelPrefab;
+
+    [SerializeField]
+    GameObject btnPrefab;
+
+    [SerializeField]
+    List<AudioClip> ac = new List<AudioClip>();
+
+    AudioSource asrc;
+
+    // Use this for initialization =
+    void Start () {
+        asrc = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void stageBtnClick()
+    {
+        var pInstance = Instantiate(panelPrefab);
+        pInstance.transform.SetParent(gameObject.transform, false);
+
+        PlaySE(0);
+
+        var bIns = Instantiate(btnPrefab);
+        bIns.transform.SetParent(pInstance.transform, false);
+        //ButtonManager bm = new ButtonManager();
+        //bm.ToGameScene();
+    }
+
+    public void PlaySE(int num)
+    {
+        asrc.PlayOneShot(ac[num]);
+    }
+}
